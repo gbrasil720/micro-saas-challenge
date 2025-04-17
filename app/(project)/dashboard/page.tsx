1db5565a8567e702f9ba6e68e3c95a36a8657e70
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-04-17 14:21:11 -0300

    wip: second class

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { handleAuth } from "@/app/actions/handle-auth";
 import { auth } from "@/app/lib/auth";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
@@ -26,6 +27,7 @@ export default async function Dashboard() {
 					</button>
 				</form>
 			)}
+			<Link href="/payments">Payments</Link>
 		</div>
 	);
 }
