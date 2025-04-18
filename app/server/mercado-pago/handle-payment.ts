544cbf75fb56860d5234b6f619d3588712da5a1c
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-04-18 12:40:32 -0300

    fix: build problems

--- app/server/mercado-pago/handle-payment.ts
+++ app/server/mercado-pago/handle-payment.ts
@@ -1,9 +1,9 @@
-import { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes";
+import type { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes";
 
 export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
-  const metadata = paymentData.metadata
-  const userEmail = metadata.user_email
-  const testeId = metadata.teste_id
+	const metadata = paymentData.metadata;
+	const userEmail = metadata.user_email;
+	const testeId = metadata.teste_id;
 
-  console.log('Pagamento com Sucesso')
+	console.log("Pagamento com Sucesso", { userEmail, testeId });
 }
