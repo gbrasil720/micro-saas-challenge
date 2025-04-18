76827e0f1c3a719afc7139fe26c22779b9dca49d
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-04-18 12:55:17 -0300

    fix: success url

--- app/api/mercado-pago/pending/route.ts
+++ app/api/mercado-pago/pending/route.ts
@@ -1,26 +1,29 @@
 import { mpClient } from "@/app/lib/mercado-pago";
 import { Payment } from "mercadopago";
-import { NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url)
+	const { searchParams } = new URL(req.url);
 
-  const paymentId = searchParams.get('payment_id')
-  const testeId = searchParams.get('external_reference')
+	const paymentId = searchParams.get("payment_id");
+	const testeId = searchParams.get("external_reference");
 
-  if(!paymentId || !testeId) {
-    return NextResponse.json({ error: 'Missing payment_id or external_reference' }, { status: 400 })
-  }
+	if (!paymentId || !testeId) {
+		return NextResponse.json(
+			{ error: "Missing payment_id or external_reference" },
+			{ status: 400 },
+		);
+	}
 
-  const payment = new Payment(mpClient)
+	const payment = new Payment(mpClient);
 
-  const paymentData = await payment.get({
-    id: paymentId,
-  })
+	const paymentData = await payment.get({
+		id: paymentId,
+	});
 
-  if(paymentData.status === 'approved' || paymentData.status !== null) {
-    return NextResponse.redirect(new URL(`success`, req.url))
-  }
+	if (paymentData.status === "approved" || paymentData.status !== null) {
+		return NextResponse.redirect(new URL("/success", req.url));
+	}
 
-  return NextResponse.redirect(new URL(`/`, req.url))
+	return NextResponse.redirect(new URL("/", req.url));
 }
